6456de6281c886f3de8c47e59764a42959b61d0d
angel santillan <angelsb94@gmail.com>
2021-08-01 16:36:03 -0500

    inicio del proyecto

---
+++ src/CalendarApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppRouter from './routes/AppRouter'
+
+const CalendarApp = () => {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  )
+}
+
+export default CalendarApp
